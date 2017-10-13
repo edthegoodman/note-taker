6d4e937be260775c082013acef5c79b9e12ec26d
CORP\Edrick.deGuzman <edrick.deguzman@resolver.com>
2017-10-13 13:29:27 -0600

    Almost completed To Do List, no backend or styling

--- src/App.js
+++ src/App.js
@@ -3,13 +3,56 @@ import Comments from './Components/Comments';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+        super(props);
+        this.state = {numChildren: 0, comments: [], newCommentValue:''};
+        this.eachComment = this.eachComment.bind(this);
+        this.updateComment = this.updateComment.bind(this);
+        this.removeComment = this.removeComment.bind(this);
+        this.addComment = this.addComment.bind(this);
+        this.handleNewComment = this.handleNewComment.bind(this);
+  }
+
+  removeComment(i) {
+    console.log('Removing comments: ' + i);
+    var arr = this.state.comments;
+    arr.splice(i, 1); // remove the ith comment, the 1 stands for removing just the ith comment
+    this.setState({comments: arr}); // set the new array in the state
+  }
+
+  updateComment(newText, i) {
+    console.log('Updating comments: ' + i);
+    var arr = this.state.comments;
+    arr[i] = newText;
+    this.setState({comments: arr});
+  }
+
+  addComment(text) {
+    var arr = this.state.comments;
+    arr.push(text);
+    this.setState({comments: arr});
+    this.setState({newCommentValue: ""})
+  }
+
+  eachComment(text, i) {
+    return (
+      <Comments key={i} index={i} updateCommentText={this.updateComment.bind(this)} deleteComment={this.removeComment.bind(this)} >
+        {text}
+      </Comments>
+    );
+  }
+
+  handleNewComment(event) {
+    this.setState({newCommentValue: event.target.value})
+  }
+
   render() {
     return (
       <div className="App">
-        Let's start taking notes!
-        <Comments>This is the first text</Comments>
-        <Comments>This is the second text</Comments>
-        <Comments>This is the third text</Comments>
+        <h1>Let's start taking notes!</h1>
+        <input className="new-comment-input" type="text" value={this.state.newCommentValue} onChange={this.handleNewComment}/>
+        <button onClick={this.addComment.bind(null, this.state.newCommentValue)} className="button-info create">Add New Comment</button>
+        {this.state.comments.map(this.eachComment)}
       </div>
     );
   }
--- src/Components/Comments.js
+++ src/Components/Comments.js
@@ -4,9 +4,11 @@ class Comments extends Component {
   
     constructor(props) {
         super(props);
-        this.state = {editing: false};
+        this.state = {editing: false, complete: false};
         this.edit = this.edit.bind(this);
         this.save = this.save.bind(this);
+        this.remove = this.remove.bind(this);
+        this.complete = this.complete.bind(this);
     }
 
     edit() {
@@ -14,12 +16,19 @@ class Comments extends Component {
     }
 
     remove() {
-        alert('Removing comment');
+        this.props.deleteComment(this.props.index);
     }
 
     save() {
+        this.props.updateCommentText(this.refs.newText.value, this.props.index)
         this.setState({editing: false});
     }
+
+    complete() {
+        alert('This task has now been completed');
+        this.setState({complete: true});
+    }
+
     // 2 Different functions to render a Normal State (editing=false and editing=true)
     renderNormal() {
         return (
@@ -27,21 +36,34 @@ class Comments extends Component {
               <div className="commentText">{this.props.children}</div>
               <button onClick={this.edit} className="button-primary">Edit</button>
               <button onClick={this.remove} className="button-danger">Remove</button>
+              <button onClick={this.complete} className="button-complete">Complete</button>
           </div>
-        );}
+    );}
 
     renderForm() {
         return (
         <div className="commentContainer">
-            <textarea name="commentArea" defaultValue={this.props.children}></textarea>
+            <textarea ref="newText" name="commentArea" defaultValue={this.props.children}></textarea>
             <button onClick={this.save} className="button-save">Save</button>
         </div>
     );}
+
+    renderComplete() {
+        return (
+          <div className="commentContainer">
+              <div className="commentText">{this.props.children}</div>
+          </div>
+    );}
+
     render() {
         if(this.state.editing) {
             return this.renderForm();
         } else {
-            return this.renderNormal();
+            if(this.state.complete) {
+                return this.renderComplete();
+            } else {
+                return this.renderNormal();
+            }
         }
     }
 }
